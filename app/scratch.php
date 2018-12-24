d47bec0aa22b150e295b50bd303bd1add595d828
HOME_신형준 <ssa4141@naver.com>
2018-12-25 01:48:09 +0900

    업로드

---
+++ app/scratch.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: master
+ * Date: 2018-12-24
+ * Time: 오전 1:26
+ */
+
+function compress($pipes, $pipType, $destination, $compressor)
+{
+    if($fileType == 'css'){
+
+    }
+
+    if($fileType == 'javascript'){
+        if($compressor){
+            $compressor($files);
+        }
+    }
+}
